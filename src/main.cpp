84ed85bd560f598e343db10fecf65bcfb2ee9013
Alec Glisman <alec.glisman@gmail.com>
2021-07-29 16:29:07 -0700

    basic main file

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Alec Glisman on 3/27/20.
+//
+
+/* Include all internal project dependencies */
+
+/* Include all external project dependencies */
+// eigen3(Linear algebra)
+#include <Eigen3/Core>
+#include <Eigen3/Eigen3>
+#define EIGEN_USE_MKL_ALL
+#include <Eigen/src/Core/util/MKL_support.h>
+// STL Library
+#include <memory> // for std::unique_ptr and std::shared_ptr
+#include <string> // std::string
+#include <vector> // std::vector()
+
+/* Forward declarations */
+
+int
+main(const int argc, const char* argv[])
+{
+
+    /* SECTION: Parse command line input
+     *      argv[0]: executable name
+     *      argv[1]: input preferences filepath
+     *      argv[2]: output directory to write data to
+     *      argv[3]: (optional) initial configuration data file
+     */
+
+    // Check input is of correct length
+    // if (argc < 3)
+    // {
+
+    //     std::cout << "ERROR: incorrect number of arguments!!!"
+    //               << "[executable] [input preferences] [output directory] [(optional) initial "
+    //                  "configuration]"
+    //               << std::endl;
+
+    //     return EXIT_FAILURE;
+    // }
+
+    /* !SECTION */
+
+    return EXIT_SUCCESS;
+}
