00801ede1c82847959a79cf9de39fcbfb811f302
Alec Glisman <alec.glisman@gmail.com>
2021-07-30 16:58:09 -0700

    added eigen import

--- src/simulation_system/systemData.hpp
+++ src/simulation_system/systemData.hpp
@@ -16,7 +16,11 @@
 // Logging
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
-
+// eigen3(Linear algebra)
+#include <Eigen/Core>
+#include <Eigen/Eigen>
+#define EIGEN_USE_MKL_ALL
+#include <Eigen/src/Core/util/MKL_support.h>
 // STL
 #include <memory> // for std::unique_ptr and std::shared_ptr
 #include <string> // std::string
@@ -29,15 +33,40 @@ class systemData
     ~systemData();
 
   private:
+    // constructor
     std::string m_inputGSDFile;
     std::string m_outputDir;
 
+    // logging
     std::string                     m_logFile;
     std::string                     m_logName{"systemData"};
     std::shared_ptr<spdlog::logger> m_logger;
 
+    // GSD
     std::shared_ptr<gsd_handle> m_handle{new gsd_handle};
     int                         m_return_val{-1};
+
+    // degrees of freedom
+    int num_dim{-1};
+    int num_particles{-1};
+
+    // integrator
+    double dt{-1};
+    double tf{-1};
+    int    num_steps_output{-1};
+
+    // material parameters
+    double fluid_density{-1};
+    double particle_density{-1};
+
+    // potential parameters
+    double wca_epsilon{-1};
+    double wca_sigma{-1};
+
+    // kinematics
+    Eigen::VectorXd positions;
+    Eigen::VectorXd velocities;
+    Eigen::VectorXd accelerations;
 };
 
 #endif
