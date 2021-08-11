55318738523f0f215c0b587adcbcc9abe47bfcba
Alec Glisman <alec.glisman@gmail.com>
2021-08-11 17:06:28 +0100

    added profile directory

---
+++ profile/PROFILE.md
@@ -0,0 +1,6 @@
+# Profiling Code
+
+## MacOS
+
+Use the instruments app ([documentation](https://help.apple.com/instruments/mac/current/#/devb14ffaa5)). Hard to pass command line arguments that are required. This is made easier by calling instruments via command line rather than GUI.
+Sample bash script is in this directory.
---
+++ profile/macOS.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# NOTE: Should be called from project base directory
+
+# variables
+base_dir="$(pwd)"
+output_dir="${base_dir}/profile/MacOS"
+
+# Apple instruments arguments
+instruments_template="Time Profiler"
+trace_output="${output_dir}bodies-in-potential-flow-profile.trace"
+executable="${base_dir}/build_profile/src/bodies_in_potential_flow"
+args="${base_dir}/temp/test.gsd ${base_dir}/temp/output"
+
+# configure project
+cmake -G "Unix Makefiles" -B "build_profile" -DCMAKE_BUILD_TYPE=Profile -DENABLE_TESTING=True -DCMAKE_EXPORT_COMPILE_COMMANDS=True
+
+# build project
+cmake --build "build_profile" -j
+
+# profile project
+instruments -t "${instruments_template}" -D ${trace_output} ${executable} ${args}
