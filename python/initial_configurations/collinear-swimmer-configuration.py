678f7f2f0878ce928066d3fddb3bd38b29070636
Alec Glisman <alec.glisman@gmail.com>
2021-08-13 13:04:25 +0100

    moving work onto new class

--- python/initial_configurations/collinear-swimmer-configuration.py
+++ python/initial_configurations/collinear-swimmer-configuration.py
@@ -1,11 +1,24 @@
+# SECTION: Dependencies
+
 # External dependencies
 import numpy as np                 # mathematical data structures
-import gsd.hoomd                   # Simulation data-structure
 import sys                         # access system data
+import os                          # access system data
 from optparse import OptionParser  # Get user input
 
+# Internal Dependencies (NOTE: Assuming file called from project base directory)
+sys.path.insert(0, os.getcwd() + '/python')
+from GSDUtil import GSDUtil  # noqa: E402
+
+# !SECTION (Dependencies)
+
+
 # SECTION: Parse user input options
+
 parser = OptionParser()
+parser.add_option("--GSD-path", dest="u_gsd_path",
+                  help="Path to GSD file to be created",
+                  metavar="string")
 parser.add_option("--dt", dest="u_dt",
                   help="(dimensionless) time-step for numerical integration",
                   metavar="double")
@@ -18,120 +31,102 @@ parser.add_option("--phase_angle", dest="u_phase_angle",
 parser.add_option("--epsilon", dest="u_epsilon",
                   help="(dimensionless) relative oscillation amplitude compared to average inter-particle separation",
                   metavar="double")
+
 # !SECTION (Parse user input options)
 
 
 # SECTION: Parameters
-# Simulation parameters
-gsd_name = "test.gsd"
-step = np.array([0], dtype=np.uint64)
-num_dimensions = np.array([3], dtype=np.uint8)
+
+# Swimmer parameters
+omega = 2.0 * np.pi
+tau = (2.0 * np.pi) / omega
 
 # Integrator parameters
-dt = np.array([1e-5], dtype=np.float32)
-t = np.array([0.0], dtype=np.float32)
-tf = np.array([1.0], dtype=np.float32)
-tau = np.array([1.0], dtype=np.float32)
-num_steps_output = np.array([1000], dtype=np.uint64)
-
-# Length scales
-a = 1.0  # NOTE[epic=assumptions,seq=1]: a must always be 1
-wall_separation = 10 * a
-box_size = 30 * a
+t = 0.0
+tf = 1.0
+num_steps_output = 1000
 
 # Material parameters
-fluid_density = np.array([1.0], dtype=np.float32)
-particle_density = np.array([1.0], dtype=np.float32)
+fluid_density = 1.0
+particle_density = 1.0
 
 # Potential parameters
-wca_epsilon = np.array([0.0], dtype=np.float32)
-wca_sigma = np.array([3.0*a], dtype=np.float32)
+wca_epsilon = 0.0
+wca_sigma = 3.0
 
-# Initial positions
-R_1 = np.array([5.0, 0.0, wall_separation], dtype=np.float32)
-R_2 = np.array([0.0, 0.0, wall_separation], dtype=np.float32)
-R_3 = np.array([-5.0, 0.0, wall_separation], dtype=np.float32)
-R_4 = np.array([5.0, 0.0, -wall_separation], dtype=np.float32)
-R_5 = np.array([0.0, 0.0, -wall_separation], dtype=np.float32)
-R_6 = np.array([-5.0, 0.0, -wall_separation], dtype=np.float32)
+# Particle parameters
+n = 3
+types = ['Swimmer']
+typeid = [0, 0, 0]
 
-# Initial velocities
-U_1 = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-U_2 = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-U_3 = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-U_4 = np.array([1.0, 0.0, -0.0], dtype=np.float32)
-U_5 = np.array([1.0, 0.0, -0.0], dtype=np.float32)
-U_6 = np.array([1.0, 0.0, -0.0], dtype=np.float32)
-
-# Initial accelerations
-A_1 = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-A_2 = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-A_3 = np.array([1.0, 0.0, 0.0], dtype=np.float32)
-A_4 = np.array([1.0, 0.0, -0.0], dtype=np.float32)
-A_5 = np.array([1.0, 0.0, -0.0], dtype=np.float32)
-A_6 = np.array([1.0, 0.0, -0.0], dtype=np.float32)
 # !SECTION (Parameters)
 
 
-# SECTION: Snapshot initialization
-s = gsd.hoomd.Snapshot()
+# SECTION: GSD Creation
 
-s.configuration.step = step  # simulation time step
-# number of spatial dimensions in simulation
-s.configuration.dimensions = num_dimensions
-# simulation box size (periodic)
-#   box[0:3]: (𝑙𝑥,𝑙𝑦,𝑙𝑧)  the box length in each direction, in length units
-#   box[3:]: (𝑥𝑦, 𝑥𝑧, 𝑦𝑧) the tilt factors, unitless values
-s.configuration.box = [box_size, box_size, box_size, 0, 0, 0]
+def initializeGSD():
+    global gsd_class
 
-# Needed, as GSD does not have these saved by default for some reason?
-s.log['configuration/step'] = step
-s.log['configuration/dimensions'] = num_dimensions
-
-s.log['integrator/dt'] = dt
-s.log['integrator/t'] = t
-s.log['integrator/tf'] = tf
-s.log['integrator/tau'] = tau
-s.log['integrator/num_steps_output'] = num_steps_output
-
-s.log['material_parameters/fluid_density'] = fluid_density
-s.log['material_parameters/particle_density'] = particle_density
-
-s.log['wca/epsilon'] = wca_epsilon
-s.log['wca/sigma'] = wca_sigma
-
-s.particles.N = 6  # number of particles
-s.particles.types = ['Squirmer', 'Image']  # number of particle types
-s.particles.typeid = [0, 0, 0,  # Type-ID of each particle
-                      1, 1, 1]
-s.particles.diameter = [2*a, 2*a, 2*a,  # diameter
-                        2*a, 2*a, 2*a]
-
-s.particles.position = [  # initial positions
-    R_1, R_2, R_3, R_4, R_5, R_6
-]
-s.particles.velocity = [  # initial velocities
-    U_1, U_2, U_3, U_4, U_5, U_6
-]
-# NOTE[epic=data,seq=1]: Acceleration data is being stored in
-# moment_inertia field as there is not a proper storage field for such data
-s.particles.moment_inertia = [  # initial accelerations
-    A_1, A_2, A_3, A_4, A_5, A_6
-]
-# !SECTION (Snapshot initialization)
+    gsd_class = GSDUtil(gsd_path, create_gsd=True)
+    gsd_class.setLogParameters(dt, t, tf, tau, num_steps_output,
+                               fluid_density, particle_density, wca_epsilon, wca_sigma)
+    gsd_class.setParticleParameters(n, types, typeid)
 
 
-# SECTION: Trajectory initialization
-traj = gsd.hoomd.open(name=gsd_name, mode='wb')
-traj.append(s)
-# !SECTION: (Trajectory initialization)
+def setInitialConditions():
 
-# For use when being called from command line
+    # position
+    pos = np.zeros((3*n), dtype=np.float32)
+
+    # velocity
+    vel = np.zeros_like(pos)
+
+    # acceleration
+    acc = np.zeros_like(vel)
+
+    # output data
+    gsd_class.setKinematics(pos, vel, acc)
+
+
+def setSystemData():
+
+    # get snapshot
+    snapshot = gsd_class.snapshot
+
+    # convert data to GSD expected type
+    l_R_avg = np.array([R_avg], dtype=np.float32)
+    l_U0 = np.array([U0], dtype=np.float32)
+    l_omega = np.array([omega], dtype=np.float32)
+    l_phase_angle = np.array([phase_angle], dtype=np.float32)
+
+    # output data
+    snapshot.log['swimmer/R_avg'] = l_R_avg
+    snapshot.log['swimmer/U0'] = l_U0
+    snapshot.log['swimmer/omega'] = l_omega
+    snapshot.log['swimmer/phase_shift'] = l_phase_angle
+
+# !SECTION: (GSD Creation)
+
+
+# SECTION: For use when being called from command line
+
 if __name__ == "__main__":
+    global gsd_path, dt, R_avg, phase_angle, epsilon, U0
 
+    # Parse user input
     options, remainder = parser.parse_args(sys.argv[1:])
-
+    gsd_path = str(options.u_gsd_path)
     dt = np.double(options.u_dt)
     R_avg = np.double(options.u_R_avg)
     phase_angle = np.double(options.u_phase_angle)
     epsilon = np.double(options.u_epsilon)
+
+    # Calculate parameters
+    U0 = epsilon * R_avg * omega
+
+    initializeGSD()         # Create GSD, set log parameters, set particle parameters
+    setInitialConditions()  # set kinematics
+    setSystemData()         # set data specific to given simulation system
+    gsd_class.saveSnapshot()
+
+# !SECTION
