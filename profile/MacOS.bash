2aadcfeea050d2415b8f40decff486dbdbf1933d
Alec Glisman <alec.glisman@gmail.com>
2021-08-23 19:35:37 -0700

    changed argument for build

--- profile/MacOS.bash
+++ profile/MacOS.bash
@@ -10,7 +10,7 @@ output_dir="${base_dir}/profile/MacOS"
 instruments_template="Time Profiler"
 trace_output="${output_dir}/bodies-in-potential-flow-profile.trace"
 executable="${base_dir}/build_profile/src/bodies_in_potential_flow"
-args="${base_dir}/temp/test.gsd ${base_dir}/temp/output"
+args="${base_dir}/input/starter_gsd/collinear_swimmer_wall.gsd ${base_dir}/temp/output"
 
 # configure project
 cmake -G "Unix Makefiles" -B "build_profile" -DCMAKE_BUILD_TYPE=Profile -DENABLE_TESTING=True -DCMAKE_EXPORT_COMPILE_COMMANDS=True
