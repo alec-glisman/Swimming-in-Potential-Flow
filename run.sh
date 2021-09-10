e21e9c6d989737b02c5937f8cdbb4a50ea817e6f
Alec Glisman <alec.glisman@gmail.com>
2021-09-10 11:41:33 -0700

    updated documentation on script

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
-#!/usr/bin/env bash --login
+#!/usr/bin/env zsh --login
+
+# Script to activate python environment (for data analysis) and
+# run perl script (to build and call C++ project).
+#
+# Command line arguments:
+#     $0: ./run.sh
+#     $1:  filename of perl script to call (no extension or prefix)
+#
+# EXAMPLE: ./run.sh collinear-swimmer-wall
+
+# exit immediately if a command exits with a non-zero status.
 set -e
 
-# activate conda environment and let the following process take over
+# activate conda environment
 conda activate bodies-in-potential-flow
-exec scripts/"$@".pl
+
+# run script in shell
+exec perl scripts/"$@".pl
