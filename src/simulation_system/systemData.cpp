ba779b9c0c633f99fc6adca63576a8557acd0dbf
Alec Glisman <alec.glisman@gmail.com>
2021-10-06 11:15:28 -0700

    added more input checking assertions

--- src/simulation_system/systemData.cpp
+++ src/simulation_system/systemData.cpp
@@ -24,12 +24,12 @@ systemData::~systemData()
     spdlog::get(m_logName)->flush();
 }
 
-
 void
 systemData::parseGSD()
 {
     spdlog::get(m_logName)->info("Starting to parse GSD file");
 
+    // parse GSD file and load data into *this
     m_gsdUtil    = std::make_shared<GSDUtil>(shared_from_this());
     m_GSD_parsed = true;
 
@@ -57,20 +57,34 @@ void
 systemData::checkInput()
 {
     spdlog::get(m_logName)->info("Input checking assertions");
+
+    assert(m_positions.size() == 4 * m_num_particles &&
+           "Orientation (unit quaternions) vector has incorrect length, not 4N.");
+    assert(m_positions.size() == 3 * m_num_particles &&
+           "Position vector has incorrect length, not 3N.");
+    assert(m_velocities.size() == 3 * m_num_particles &&
+           "Velocity vector has incorrect length, not 3N.");
+    assert(m_accelerations.size() == 3 * m_num_particles &&
+           "Acceleration vector has incorrect length, not 3N.");
+
+    assert(m_particle_type_id.size() == m_num_particles &&
+           "Particle type ID vector has incorrect length, not N.");
+
+    assert(m_num_particles > 0 && "Must have at least one particle to simulate.");
+    assert(m_num_bodies > 0 && "Must have at least one body to simulate.");
+    assert(m_num_spatial_dim == 3 &&
+           "Simulation framwork currently requires 3 spatial dimensions.");
+
+    assert(m_tf > 0. && "Must have positive integration time.");
+    assert(m_dt > 0.0 && "Integration time step must be positive.");
+    assert(m_tf > m_t && "Final time must be greater than current time.");
+    assert(m_tau > 0.0 && "Characteristic time must be greater than zero.");
+
     assert(m_fluid_density >= 0.0 && "Fluid density must be non-negative.");
     assert(m_particle_density >= 0.0 && "Particle density must be non-negative");
-    assert(m_num_particles > 0 && "Must have at least one particle to simulate.");
-    assert(m_tf > 0. && "Must have positive integration time.");
-    assert(m_dt > 0.0 && "Integration time step must be positive");
-    assert(m_wca_epsilon >= 0.0 && "WCA_epsilon must be non-negative");
-    assert(m_wca_sigma >= 0.0 && "WCA_sigma must be non-negative");
 
-    assert(m_positions.size() == m_num_DoF * m_num_particles &&
-           "Position vector has incorrect length, not 3N");
-    assert(m_velocities.size() == m_num_DoF * m_num_particles &&
-           "Velocity vector has incorrect length, not 3N");
-    assert(m_accelerations.size() == m_num_DoF * m_num_particles &&
-           "Acceleration vector has incorrect length, not 3N");
+    assert(m_wca_epsilon >= 0.0 && "WCA_epsilon must be non-negative.");
+    assert(m_wca_sigma >= 0.0 && "WCA_sigma must be non-negative.");
 }
 
 void
