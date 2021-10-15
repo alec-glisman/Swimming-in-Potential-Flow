10bbcb69e8d70ea9fd5566185392699caef78d9f
Alec Glisman <alec.glisman@gmail.com>
2021-10-15 13:48:51 -0700

    updated documentation

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -24,58 +24,59 @@ var NAVTREE =
 [
   [ "Bodies in Potential Flow", "index.html", [
     [ "README: Bodies in Potential Flow", "index.html", [
-      [ "Software tested", "index.html#autotoc_md1", null ],
+      [ "Code documentation", "index.html#autotoc_md1", null ],
       [ "Docker integration", "index.html#autotoc_md2", null ],
       [ "Project structure: links to relevant readme files", "index.html#autotoc_md3", null ],
       [ "Data I/O", "index.html#autotoc_md4", null ],
-      [ "Modifications for other systems", "index.html#autotoc_md5", null ],
+      [ "Modify repository for other potential flow systems", "index.html#autotoc_md5", null ],
       [ "External dependencies", "index.html#autotoc_md6", [
-        [ "Must install separately", "index.html#autotoc_md7", null ],
-        [ "Integrated into project", "index.html#autotoc_md8", null ]
+        [ "Software tested", "index.html#autotoc_md7", null ],
+        [ "Must install separately", "index.html#autotoc_md8", null ],
+        [ "Integrated into project", "index.html#autotoc_md9", null ]
       ] ]
     ] ],
     [ "Directory: requirements", "md_requirements_README.html", [
-      [ "Subdirectory: C++", "md_requirements_README.html#autotoc_md10", null ],
-      [ "Subdirectory: Perl", "md_requirements_README.html#autotoc_md11", null ],
-      [ "Subdirectory: Python", "md_requirements_README.html#autotoc_md12", null ]
+      [ "Subdirectory: C++", "md_requirements_README.html#autotoc_md11", null ],
+      [ "Subdirectory: Perl", "md_requirements_README.html#autotoc_md12", null ],
+      [ "Subdirectory: Python", "md_requirements_README.html#autotoc_md13", null ]
     ] ],
     [ "Directory: src", "md_src_README.html", [
-      [ "Subdirectory: cuda_helpers", "md_src_README.html#autotoc_md15", null ],
-      [ "Subdirectory: data_io", "md_src_README.html#autotoc_md17", [
-        [ "Class: gsd", "md_src_README.html#autotoc_md18", null ],
-        [ "Class: GSDUtil", "md_src_README.html#autotoc_md19", null ]
+      [ "Subdirectory: cuda_helpers", "md_src_README.html#autotoc_md16", null ],
+      [ "Subdirectory: data_io", "md_src_README.html#autotoc_md18", [
+        [ "Class: gsd", "md_src_README.html#autotoc_md19", null ],
+        [ "Class: GSDUtil", "md_src_README.html#autotoc_md20", null ]
       ] ],
-      [ "Subdirectory: forces", "md_src_README.html#autotoc_md21", [
-        [ "Class: potentialHydrodynamics", "md_src_README.html#autotoc_md22", null ]
+      [ "Subdirectory: forces", "md_src_README.html#autotoc_md22", [
+        [ "Class: potentialHydrodynamics", "md_src_README.html#autotoc_md23", null ]
       ] ],
-      [ "Subdirectory: integrators", "md_src_README.html#autotoc_md24", [
-        [ "Class: rungeKutta4", "md_src_README.html#autotoc_md25", null ]
+      [ "Subdirectory: integrators", "md_src_README.html#autotoc_md25", [
+        [ "Class: rungeKutta4", "md_src_README.html#autotoc_md26", null ]
       ] ],
-      [ "Subdirectory: simulation_system", "md_src_README.html#autotoc_md27", [
-        [ "Class: engine", "md_src_README.html#autotoc_md28", null ],
-        [ "Class: progressBar", "md_src_README.html#autotoc_md29", null ],
-        [ "Class: systemData", "md_src_README.html#autotoc_md30", null ]
+      [ "Subdirectory: simulation_system", "md_src_README.html#autotoc_md28", [
+        [ "Class: engine", "md_src_README.html#autotoc_md29", null ],
+        [ "Class: progressBar", "md_src_README.html#autotoc_md30", null ],
+        [ "Class: systemData", "md_src_README.html#autotoc_md31", null ]
       ] ],
-      [ "Files", "md_src_README.html#autotoc_md32", null ]
+      [ "Files", "md_src_README.html#autotoc_md33", null ]
     ] ],
     [ "Directory: tests", "md_tests_README.html", null ],
     [ "Directory: python", "md_python_README.html", [
-      [ "Subdirectory: analysis", "md_python_README.html#autotoc_md35", null ],
-      [ "Subdirectory: initial_configurations", "md_python_README.html#autotoc_md36", null ],
-      [ "Files", "md_python_README.html#autotoc_md37", null ]
+      [ "Subdirectory: analysis", "md_python_README.html#autotoc_md36", null ],
+      [ "Subdirectory: initial_configurations", "md_python_README.html#autotoc_md37", null ],
+      [ "Files", "md_python_README.html#autotoc_md38", null ]
     ] ],
     [ "Directory: profile", "md_profile_README.html", [
-      [ "Subdirectory (submodule): <a href=\"https://github.com/brendangregg/FlameGraph\">FlameGraph</a>", "md_profile_README.html#autotoc_md39", null ],
-      [ "Subdirectory: Linux", "md_profile_README.html#autotoc_md40", null ],
-      [ "Subdirectory: MacOS", "md_profile_README.html#autotoc_md41", null ]
+      [ "Subdirectory (submodule): <a href=\"https://github.com/brendangregg/FlameGraph\">FlameGraph</a>", "md_profile_README.html#autotoc_md40", null ],
+      [ "Subdirectory: Linux", "md_profile_README.html#autotoc_md41", null ],
+      [ "Subdirectory: MacOS", "md_profile_README.html#autotoc_md42", null ]
     ] ],
     [ "Project Development Progress", "md_TODO.html", [
-      [ "Get project running", "md_TODO.html#autotoc_md43", null ],
-      [ "Updated hydrodynamics", "md_TODO.html#autotoc_md44", null ],
-      [ "Tensor module optimization", "md_TODO.html#autotoc_md45", null ],
-      [ "Bugs to Fix", "md_TODO.html#autotoc_md46", null ],
-      [ "Verifications", "md_TODO.html#autotoc_md47", null ],
-      [ "Additional features", "md_TODO.html#autotoc_md48", null ]
+      [ "Get project running", "md_TODO.html#autotoc_md44", null ],
+      [ "Updated hydrodynamics", "md_TODO.html#autotoc_md45", null ],
+      [ "Tensor module optimization", "md_TODO.html#autotoc_md46", null ],
+      [ "Bugs to Fix", "md_TODO.html#autotoc_md47", null ],
+      [ "Verifications", "md_TODO.html#autotoc_md48", null ],
+      [ "Additional features", "md_TODO.html#autotoc_md49", null ]
     ] ],
     [ "Todo List", "todo.html", null ],
     [ "Altering Swimmer Design List", "review.html", null ],
