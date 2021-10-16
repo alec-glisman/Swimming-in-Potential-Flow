1bf348282c1186bf600aa688d524a22899aa153a
Alec Glisman <alec.glisman@gmail.com>
2021-10-16 14:37:56 -0700

    updated formatting to not throw errors

--- tests/testsSimulationBuild.cpp
+++ tests/testsSimulationBuild.cpp
@@ -17,26 +17,26 @@
 
 TEST_CASE("Load and parse data from GSD file", "[GSD][I/O]")
 {
-    /* Parameters
-     * REVIEW[epic=Assumptions]: Assuming that the tests are called from project base dir */
+    /* I/O Parameters */
     std::string inputDataFile = "input/data.gsd";
     std::string outputDir     = "output";
 
-    /* Initialize variables */
-    std::shared_ptr<gsd_handle> handle{new gsd_handle};
-    int                         return_val{-1};
-
-    std::shared_ptr<systemData> system;
-    std::shared_ptr<engine>     eng;
-
     SECTION("Load GSD data")
     {
+        /* Initialize variables */
+        std::shared_ptr<gsd_handle> handle{new gsd_handle};
+        int                         return_val{-1};
+
         REQUIRE_NOTHROW(return_val = gsd_open(handle.get(), inputDataFile.c_str(), GSD_OPEN_READONLY));
         REQUIRE(return_val == 0);
     }
 
     SECTION("Initialize simulation")
     {
+        /* Initialize variables */
+        std::shared_ptr<systemData> system;
+        std::shared_ptr<engine>     eng;
+
         REQUIRE_NOTHROW(system = std::make_shared<systemData>(inputDataFile, outputDir));
         REQUIRE_NOTHROW(system->initializeData());
 
