ea597af77b79faf387da8ca2b5f8ac838e1cb6cc
Alec <30010253+alec-glisman@users.noreply.github.com>
2021-11-09 10:14:32 -0800

    updated macOS profile script for new system

--- profile/MacOS/MacOS.bash
+++ profile/MacOS/MacOS.bash
@@ -9,14 +9,14 @@ output_dir="${base_dir}/profile/MacOS"
 # Apple instruments arguments
 instruments_template="Time Profiler"
 trace_output="${output_dir}/bodies-in-potential-flow-profile.trace"
-executable="${base_dir}/build_profile/src/bodies-in-potential-flow"
-args="${base_dir}/input/starter_gsd/collinear_swimmer_wall.gsd ${output_dir}/output"
+executable="${base_dir}/build/profile/src/bodies-in-potential-flow"
+args="${base_dir}/test/input/collinear_swimmer_isolated/initial_frame_dt1e-2.gsd ${output_dir}/output"
 
 # configure project
-cmake -G "Unix Makefiles" -B "build_profile" -DCMAKE_BUILD_TYPE=Profile -DENABLE_TESTING=True -DCMAKE_EXPORT_COMPILE_COMMANDS=True
+cmake -G "Ninja" -B "build/profile" -DCMAKE_BUILD_TYPE=Profile -DENABLE_TESTING=False -DENABLE_COVERAGE=False
 
 # build project
-cmake --build "build_profile" -j
+cmake --build "build/profile" -j
 
 # profile project
 instruments -t "${instruments_template}" -D ${trace_output} ${executable} ${args}
