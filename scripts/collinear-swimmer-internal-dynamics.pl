c898ed149db1058a284dc624d43131adf952cba5
Alec <30010253+alec-glisman@users.noreply.github.com>
2021-11-15 12:41:15 -0800

    larger oscillation amplitude

--- scripts/collinear-swimmer-internal-dynamics.pl
+++ scripts/collinear-swimmer-internal-dynamics.pl
@@ -207,7 +207,7 @@ for (my $i = 0; $i < $numSimulationTypes; $i += 1 ){
 		my $Z_height    = 6.00e+0;
 
 		my $phase_angle = -1.57079632679e+0;
-		my $U0          = 1.00e-2;
+		my $U0          = 1.00e-0;
 		my $omega       = 1.00e+0;
 
 		my $number_bodies = 2;
