ba20a7f6e7eaf6f06eb858631c8201b938f3f8a8
Alec Glisman <30010253+alec-glisman@users.noreply.github.com>
2021-12-08 15:47:00 -0800

    updated script to accept integer giving initial orientation. Also calculating image system quaternion correctly now

--- python/initial_configurations/collinear-swimmer-internal-dynamics-configuration.py
+++ python/initial_configurations/collinear-swimmer-internal-dynamics-configuration.py
@@ -59,8 +59,13 @@ parser.add_option("--number-bodies", dest="u_number_bodies",
                   help="number of bodies to simulate",
                   metavar="int")
 parser.add_option("--image-system", dest="u_image_system",
-                  help="Is the system under consideration an image system (about z-axis)",
+                  help="Is the system under consideration an image system about z-axis? (1: true, 0: false)",
                   metavar="int")
+
+parser.add_option("--orientation", dest="u_orientation",
+                  help="How are bodies initially oriented? (0: +x-axis orientation, 1: -z-axis orientation)",
+                  metavar="int")
+
 
 # !SECTION (Parse user input options)
 
@@ -142,7 +147,7 @@ def initializeGSD(gsd_path,
 def setInitialConditions(gsd_class,
                          N, M, num_particles_per_body,
                          Z_height,
-                         image_system):
+                         image_system, orientation):
     """Set initial kinematics of GSD file
 
     Args:
@@ -152,15 +157,33 @@ def setInitialConditions(gsd_class,
         num_particles_per_body (int): number of particles per body
         Z_height (float32): z-axis height of bodies
         image_system (int): Integer boolean deciding if simulation is an image system
+        orientation (int): Integer giving initial orientation of bodies (see parser input for more detail)
     """
 
-    # orientation
+    # rotation tensor
     quat = np.zeros((N, 4), dtype=np.float32)
+    # rotate 0 radians
     no_rotation_quat = np.array([1.0, 0.0, 0.0, 0.0], dtype=np.float32)
+    # rotate -(3 \pi) / 2 radians about y-axis (0, 1 ,0): results in +x --> -z axis rotation
+    rot_x_to_nz = (1.0 / np.sqrt(2.0, dtype=np.float32)) * \
+        np.array([-1.0, 0.0, 1.0, 0.0], dtype=np.float32)
+
+    # pick quaternion to use
+    quat_used = no_rotation_quat
+
+    if (orientation == 1):
+        quat_used_img = rot_x_to_nz
+
+    # accounts for +z --> -z axis inversion for image system
+    quat_used_img[1:3] *= -1.0
 
     for i in range(N):
 
-        quat[i] = no_rotation_quat
+        if ((image_system == 1) and (i >= (N / 2))):  # image system bodies
+            quat[i] = quat_used_img
+
+        else:  # real system bodies
+            quat[i] = quat_used
 
     # position (only need to specify locater position, articulation calculated in C++ simulation)
     pos = np.zeros((N, 3), dtype=np.float32)
@@ -173,21 +196,17 @@ def setInitialConditions(gsd_class,
 
             # "Real" particles in image system
             if ((i < (M/2)) and (image_system == 1)):
-
                 pos[Ni + j] = [0.0, 0.0, Z_height]
 
             # "Image" particles in image system
             elif ((i >= (M/2)) and (image_system == 1)):
-
                 pos[Ni + j] = [0.0, 0.0, -Z_height]
 
             # All particles in non-image system
             else:
-
                 pos[Ni + j] = [0.0, 0.0, Z_height]
 
     vel = np.zeros_like(pos, dtype=np.float32)  # Calculated in C++ simulation
-
     acc = np.zeros_like(pos, dtype=np.float32)  # Calculated in C++ simulation
 
     # output data
@@ -265,8 +284,8 @@ if __name__ == "__main__":
     U0 = np.float32(options.u_U0)
     omega = np.float32(options.u_omega)
 
-    # REVIEW: {1: image system, 0: regular system}
     image_system = int(options.u_image_system)
+    orientation_init = int(options.u_orientation)
 
     M = int(options.u_number_bodies)
     N = num_particles_per_body * M
@@ -285,7 +304,7 @@ if __name__ == "__main__":
     setInitialConditions(gsd_class,
                          N, M, num_particles_per_body,
                          Z_height,
-                         image_system)
+                         image_system, orientation_init)
 
     setSystemData(gsd_class,
                   M,
