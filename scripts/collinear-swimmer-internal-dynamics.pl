90669fc8e6b9ce7d41d08e9cf1492149020b2594
Alec Glisman <30010253+alec-glisman@users.noreply.github.com>
2021-12-08 16:42:21 -0800

    removed option to varyDt

--- scripts/collinear-swimmer-internal-dynamics.pl
+++ scripts/collinear-swimmer-internal-dynamics.pl
@@ -41,7 +41,7 @@ my $simulationTag    = "collinear-swimmer-internal-dynamics";
 my $projectName      = "bodies-in-potential-flow";
 my $inputDir         = "input";
 my $runSimulationSimulan = 1; # NOTE: 0 only runs one simulation at a time
-my @inputData        = ( "varyZHeight", "varyEpsilon" );  # other options: [ "varyRelDisp",  "varyPhaseAngle",  "varyVarEpsilon", "varyDt" ]
+my @inputData        = ( "varyZHeight", "varyEpsilon" );  # other options: [ "varyRelDisp",  "varyPhaseAngle",  "varyVarEpsilon" ]
 my $numSimulationTypes = scalar @inputData;
 
 # Simulation parameters
@@ -49,6 +49,7 @@ my $boolInternalDynamics = 1; # if swimmer has internal dynamics (0: no, 1: yes)
 my $boolImageSystem      = 0; # if system has an image (wall) along z=0 plane
 my $numberBodies         = 1; # integer number of bodies to simulate
 
+my $dt          = 1.00e-6;		
 my $ti          = 0.00e+0;
 my $tf          = 1.00e+0;
 
@@ -208,8 +209,6 @@ for (my $i = 0; $i < $numSimulationTypes; $i += 1 ){
 		my $gsd_path = ${simulation_dir} . "/" . "data.gsd";
 
 		# Simulation variables
-		my $dt          = 1.00e-6;
-		
         my $R_avg       = 6.00e+0;
 		my $Z_height    = 1.00e+1;
 
