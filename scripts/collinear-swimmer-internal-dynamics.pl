700d947b5e019e2991ed71e7581bc8d682a1a22e
Alec Glisman <30010253+alec-glisman@users.noreply.github.com>
2021-12-09 16:25:54 -0800

    more simulatenous simulations

--- scripts/collinear-swimmer-internal-dynamics.pl
+++ scripts/collinear-swimmer-internal-dynamics.pl
@@ -72,7 +72,7 @@ my $numThreads = 0;
 if ((index($host, "MacBook-Pro") != -1) or (index($host, "mbp") != -1)) {
 	$numThreads = 12;
 } elsif ((index($host, "alec-glisman-PC-Ubuntu") != -1) or (index($host, "alec-glisman-PC-Windows") != -1)) {
-	$numThreads  = 8;
+	$numThreads  = 16;
 } elsif ((index($host, "stokes") != -1 )) {
 	$numThreads  = 6;
 } elsif ((index($host, "shear") != -1 ) or ( index($host, "s") != -1 )) {
