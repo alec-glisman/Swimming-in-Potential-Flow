bcafc0ae88812e9fd10857c1ab993cdc2cc2e291
Alec Glisman <30010253+alec-glisman@users.noreply.github.com>
2021-12-11 08:21:00 -0800

    less steps output

--- python/initial_configurations/collinear-swimmer-internal-dynamics-configuration.py
+++ python/initial_configurations/collinear-swimmer-internal-dynamics-configuration.py
@@ -73,7 +73,7 @@ parser.add_option("--orientation", dest="u_orientation",
 # SECTION: Parameters
 
 # Integrator parameters
-num_steps_output = 100000
+num_steps_output = 1000
 
 # Material parameters
 fluid_density = 1.0
