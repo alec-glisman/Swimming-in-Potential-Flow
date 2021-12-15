0a18f68a9273a78f60dd50ca4f983a74ed4eda4e
Alec Glisman <30010253+alec-glisman@users.noreply.github.com>
2021-12-15 09:51:58 -0800

    horizontal orientation with no internal dynamics

--- src/simulation_system/SystemData.cpp
+++ src/simulation_system/SystemData.cpp
@@ -156,8 +156,8 @@ SystemData::initializeData()
             orient_dir = -1.0; // -x placement
         }
 
-        m_positions_particles_articulation_init_norm(particle_id_3 + 2) =
-            -orient_dir; /// @review_swimmer: All of body coordinate in (+x)-axis, quaternion rotates
+        m_positions_particles_articulation_init_norm(particle_id_3 + 0) =
+            orient_dir; /// @review_swimmer: All of body coordinate in (+x)-axis, quaternion rotates
 
         spdlog::get(m_logName)->info("Particle {0} initial orientation: [{1:03.14f}, {2:03.14f}, {3:03.14f}]",
                                      particle_id + 1, m_positions_particles_articulation_init_norm(particle_id_3),
